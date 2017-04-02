6271f5f7abf6304adc7ba014a63ab5eb0e5d7faa
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2017-04-02 19:27:15 +0200

    Updating Repo

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 hugo
+cp CNAME public/
 cd public
 git add .
-git commit -m "Hugo update your website"
+git commit -m "Updating HTML"
 git push
+cd ..
+git add .
+git commit -m "Updating Repo"
